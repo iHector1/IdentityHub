1ac2e80000bea025dacdf0741333ca470c9e494d
Hector Gonzalez <hectorjosuegc@gmail.com>
2026-09-22 14:03:09 -0600

    revert: restore original app configuration

--- src/UserService/IdentityHub.UserService.Api/Program.cs
+++ src/UserService/IdentityHub.UserService.Api/Program.cs
@@ -6,7 +6,6 @@ using IdentityHub.UserService.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseNpgsql(
